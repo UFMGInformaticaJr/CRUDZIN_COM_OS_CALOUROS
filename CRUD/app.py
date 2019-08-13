f2d5710710bf0c2fad6bba4021f668499281a196
Daniel <neiva.daniel@outlook.com>
2019-08-13 16:39:38 -0300

    Filtrar posts por relevancia

--- CRUD/app.py
+++ CRUD/app.py
@@ -1,38 +1,61 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 import os
+from functools import wraps
+from flask import Flask, render_template, redirect, request, flash, url_for, current_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager, current_user
+
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+############# CRIANDO BANCO DE DADOS ###############
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+
+############### CRIANDO TABELAS ##############
 class Post(db.Model):
-
+    __tablename__="posts"
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.Text)
     descricao = db.Column(db.Text)
+    tema_cor = db.Column(db.String)
 
-@app.route("/")
-def index():
 
-    posts =  Post.query.all()
+
+################ DEFININDO ROTAS #################
+
+
+@app.route("/<string:tema>")
+@app.route("/", defaults = {'tema':None})
+def index(tema):
+    if tema== "grey" or tema== None:
+        posts =  Post.query.all()
+    else:
+        posts =  Post.query.filter_by(tema_cor = tema)
     return render_template('dashboard.html', posts=posts)
 
 @app.route("/adicionar", methods=["POST"])
 def adicionar():
     titulo = request.form['titulo']
     descricao = request.form['descricao']
+    categ = request.form['categoria']
 
     novoPost = Post()
     novoPost.descricao = descricao
     novoPost.titulo  = titulo
+    novoPost.tema_cor = categ
 
     db.session.add(novoPost)
     db.session.commit()
 
-    return redirect(url_for("index"))
+    return redirect(url_for('index'))
     
 
 @app.route("/deletar/<int:id>")
@@ -55,6 +78,9 @@ def editar(id):
     db.session.commit()
 
     return redirect(url_for("index"))
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
